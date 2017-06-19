faa97fda19c5e70dd7954de5232726e6f58c14b0
Lawrence Dakai <mrlawrencedakai@gmail.com>
2017-06-19 22:11:06 +0200

    Create hello.ps1

---
+++ hello.ps1
@@ -0,0 +1,6 @@
+# The code is from: https://technet.microsoft.com/en-us/library/bb963733.aspx
+# Filename: hello.ps1
+Write-Host
+Write-Host 'Hello World!'
+Write-Host "Good-bye World! `n"
+# end of script
